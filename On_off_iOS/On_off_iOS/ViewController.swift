e5163a6674036a726ce293b2c031c0843df23ad8
JJ <jeonghojin03@gmail.com>
2023-12-31 17:57:51 +0900

    Init

---
+++ On_off_iOS/On_off_iOS/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  On_off_iOS
+//
+//  Created by 정호진 on 12/31/23.
+//
+
+import UIKit
+
+final class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .white
+    }
+
+
+}
+
