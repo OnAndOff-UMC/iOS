268b268b5623ad2b672d10a5c8b22116b7c0405d
yeahzxnn <yejin0621@tukorea.ac.kr>
2024-01-01 23:58:28 +0900

    feat : fix error and webhook test

--- On_off_iOS/On_off_iOS/Home/HomeView/HomeViewController.swift
+++ On_off_iOS/On_off_iOS/Home/HomeView/HomeViewController.swift
@@ -38,8 +38,8 @@ class HomeViewController: UIViewController {
         let collectionView = UICollectionView(frame: .zero, collectionViewLayout: layout)
         collectionView.showsHorizontalScrollIndicator = false
         collectionView.alwaysBounceHorizontal = true
-        collectionView.delegate = self
-        collectionView.dataSource = self
+        //        collectionView.delegate = self
+//        collectionView.dataSource = self
         return collectionView
     }()
     
@@ -51,8 +51,8 @@ class HomeViewController: UIViewController {
     }()
     private lazy var tableView: UITableView = {
         let tableView = UITableView()
-//        tableView.delegate = self
-//        tableView.dataSource = self
+        //        tableView.delegate = self
+        //        tableView.dataSource = self
         tableView.isScrollEnabled = false
         return tableView
     }()
@@ -65,7 +65,7 @@ class HomeViewController: UIViewController {
         topView.addSubview(dateCollectionView)
         bottomView.addSubview(tableView)
         
-//        bottomView.addSubview()
+        //        bottomView.addSubview()
         
         configureConstraints()
     }
@@ -74,9 +74,9 @@ class HomeViewController: UIViewController {
         super.viewDidLoad()
         view.backgroundColor = .white
         addSubviews()
-//        setTableView()
-//        setCollectionView()
-        scrollView.delegate = self
+        //        setTableView()
+        //        setCollectionView()
+        //        scrollView.delegate = self
         
         //
         //        let scrollView = UIScrollView()
@@ -151,123 +151,124 @@ class HomeViewController: UIViewController {
         }
     }
     
-//    private func calculateTableViewHeight() -> CGFloat {
-//        
-//        let numberOfCells =  homeGoodsDataArray.count //-> 오늘의 할일 셀 때
-//        let cellHeight: CGFloat = view.frame.width*0.3
-//        let totalHeight = CGFloat(numberOfCells-1) * cellHeight + view.frame.width*0.8
-//        return totalHeight
-//    }
-//    
-//    // 테이블뷰 높이를 계산하고 업데이트하는 메서드
-//    private func updateTableViewHeight() {
-//        let newHeight = calculateTableViewHeight()
-//        tableView.snp.updateConstraints { make in
-//            make.height.equalTo(newHeight)
-//        }
-//        scrollView.layoutIfNeeded()
-//        tableView.reloadData() // 테이블뷰를 다시 로드
-//    }
+    //    private func calculateTableViewHeight() -> CGFloat {
+    //
+    //        let numberOfCells =  homeGoodsDataArray.count //-> 오늘의 할일 셀 때
+    //        let cellHeight: CGFloat = view.frame.width*0.3
+    //        let totalHeight = CGFloat(numberOfCells-1) * cellHeight + view.frame.width*0.8
+    //        return totalHeight
+    //    }
+    //
+    //    // 테이블뷰 높이를 계산하고 업데이트하는 메서드
+    //    private func updateTableViewHeight() {
+    //        let newHeight = calculateTableViewHeight()
+    //        tableView.snp.updateConstraints { make in
+    //            make.height.equalTo(newHeight)
+    //        }
+    //        scrollView.layoutIfNeeded()
+    //        tableView.reloadData() // 테이블뷰를 다시 로드
+    //    }
     
-//    private func setCollectionView() {
-//        datecollectionView.register(HomeCategoryCollectionViewCell.self, forCellWithReuseIdentifier: HomeCategoryCollectionViewCell.reuseIdentifier)
-//        categoryCollectionView.backgroundColor = .clear
-//        categoryCollectionView.showsHorizontalScrollIndicator = false
-//    }
-//    
-//    //테이블뷰 세팅
-//    func setTableView(){
-//        tableView.register(HomeTableViewCell.self, forCellReuseIdentifier: HomeTableViewCell.reuseIdentifier)
-//        tableView.register(HomeAdTableViewCell.self, forCellReuseIdentifier: HomeAdTableViewCell.reuseIdentifier)
-//        
-//    }
-//    
+    //    private func setCollectionView() {
+    //        datecollectionView.register(HomeCategoryCollectionViewCell.self, forCellWithReuseIdentifier: HomeCategoryCollectionViewCell.reuseIdentifier)
+    //        categoryCollectionView.backgroundColor = .clear
+    //        categoryCollectionView.showsHorizontalScrollIndicator = false
+    //    }
+    //
+    //    //테이블뷰 세팅
+    //    func setTableView(){
+    //        tableView.register(HomeTableViewCell.self, forCellReuseIdentifier: HomeTableViewCell.reuseIdentifier)
+    //        tableView.register(HomeAdTableViewCell.self, forCellReuseIdentifier: HomeAdTableViewCell.reuseIdentifier)
+    //
+    //    }
+    //
     
     
     
+    //}
+    
+    //extension HomeViewController: UICollectionViewDelegate, UICollectionViewDataSource, UICollectionViewDelegateFlowLayout {
+    //    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+    ////        return homeCagetoryDataArray.count
+    ////    }
+    //
+    //    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+    ////        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: HomeCategoryCollectionViewCell.reuseIdentifier, for: indexPath) as! HomeCategoryCollectionViewCell
+    ////        cell.imageView.image = homeCagetoryDataArray[indexPath.row].image
+    ////        cell.label.text = homeCagetoryDataArray[indexPath.row].text
+    ////        return cell
+    ////    }
+    //
+    //    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, insetForSectionAt section: Int) -> UIEdgeInsets {
+    //        return UIEdgeInsets(top: 0, left: 10, bottom: 0, right: 0)
+    //    }
+    //
+    //    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumLineSpacingForSectionAt section: Int) -> CGFloat {
+    //        return 0
+    //    }
+    ////    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+    ////
+    ////
+    ////        if (homeCagetoryDataArray[indexPath.row].text == "" ){
+    ////            let cellHeight: CGFloat = collectionView.frame.height * 0.85
+    ////
+    ////            return CGSize(width: cellHeight, height: cellHeight)
+    ////        }
+    ////        else{
+    ////            let cellPadding: CGFloat = 15
+    //////            let text = homeCagetoryDataArray[indexPath.row].text
+    ////
+    ////            // 라벨 폰트를 설정
+    ////            let font = UIFont.systemFont(ofSize: 12, weight: .regular)
+    ////            // 라벨의 예상 크기를 계산
+    ////            let labelSize = (text as NSString).size(withAttributes: [
+    ////                NSAttributedString.Key.font: font
+    ////            ])
+    ////            let cellWidth = labelSize.width*2 + 2 * cellPadding
+    ////            let cellHeight: CGFloat = collectionView.frame.height * 0.85
+    ////
+    ////            return CGSize(width: cellWidth, height: cellHeight)
+    ////        }
+    ////
+    ////    }
+    //}
+    //
+    ////extension HomeViewController: UITableViewDataSource,UITableViewDelegate {
+    ////    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+    ////
+    //////        return homeGoodsDataArray.count
+    ////    }
+    ////
+    ////
+    //////    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+    //////        if indexPath.row == 6{
+    //////            let cell = tableView.dequeueReusableCell(withIdentifier: HomeAdTableViewCell.reuseIdentifier, for: indexPath) as! HomeAdTableViewCell
+    //////            return cell
+    //////        }else {
+    //////            let cell = tableView.dequeueReusableCell(withIdentifier: HomeTableViewCell.reuseIdentifier, for: indexPath) as! HomeTableViewCell
+    //////            cell.goodsImage.image = homeGoodsDataArray[indexPath.row].goodsImage
+    //////            cell.goodsTitle.text = homeGoodsDataArray[indexPath.row].goodsTitle
+    //////            cell.locationLabel.text = homeGoodsDataArray[indexPath.row].locationLabel
+    //////            cell.goodsPrice.text = homeGoodsDataArray[indexPath.row].goodsPrice
+    //////            cell.ratingCustomView.talkImageView.image = homeGoodsDataArray[indexPath.row].rating.talkImage
+    //////            cell.ratingCustomView.talkCount.text = homeGoodsDataArray[indexPath.row].rating.talkNum
+    //////            cell.ratingCustomView.heartImageView.image = homeGoodsDataArray[indexPath.row].rating.heartImage
+    //////            cell.ratingCustomView.heartCount.text = homeGoodsDataArray[indexPath.row].rating.heartNum
+    //////            return cell
+    //////
+    //////        }
+    //////    }
+    ////
+    ////    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+    ////        if indexPath.row == 6{
+    ////            return view.frame.width*0.8
+    ////
+    ////        }else{
+    ////            return view.frame.width*0.37
+    ////        }
+    ////    }
+    ////
+    ////
+    ////}
+    ////
 }
-
-extension HomeViewController: UICollectionViewDelegate, UICollectionViewDataSource, UICollectionViewDelegateFlowLayout {
-    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-//        return homeCagetoryDataArray.count
-    }
-    
-    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
-//        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: HomeCategoryCollectionViewCell.reuseIdentifier, for: indexPath) as! HomeCategoryCollectionViewCell
-//        cell.imageView.image = homeCagetoryDataArray[indexPath.row].image
-//        cell.label.text = homeCagetoryDataArray[indexPath.row].text
-//        return cell
-    }
-    
-    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, insetForSectionAt section: Int) -> UIEdgeInsets {
-        return UIEdgeInsets(top: 0, left: 10, bottom: 0, right: 0)
-    }
-    
-    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumLineSpacingForSectionAt section: Int) -> CGFloat {
-        return 0
-    }
-//    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
-//        
-//        
-//        if (homeCagetoryDataArray[indexPath.row].text == "" ){
-//            let cellHeight: CGFloat = collectionView.frame.height * 0.85
-//            
-//            return CGSize(width: cellHeight, height: cellHeight)
-//        }
-//        else{
-//            let cellPadding: CGFloat = 15
-////            let text = homeCagetoryDataArray[indexPath.row].text
-//            
-//            // 라벨 폰트를 설정
-//            let font = UIFont.systemFont(ofSize: 12, weight: .regular)
-//            // 라벨의 예상 크기를 계산
-//            let labelSize = (text as NSString).size(withAttributes: [
-//                NSAttributedString.Key.font: font
-//            ])
-//            let cellWidth = labelSize.width*2 + 2 * cellPadding
-//            let cellHeight: CGFloat = collectionView.frame.height * 0.85
-//            
-//            return CGSize(width: cellWidth, height: cellHeight)
-//        }
-//        
-//    }
-}
-
-//extension HomeViewController: UITableViewDataSource,UITableViewDelegate {
-//    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-//        
-////        return homeGoodsDataArray.count
-//    }
-//    
-//    
-////    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-////        if indexPath.row == 6{
-////            let cell = tableView.dequeueReusableCell(withIdentifier: HomeAdTableViewCell.reuseIdentifier, for: indexPath) as! HomeAdTableViewCell
-////            return cell
-////        }else {
-////            let cell = tableView.dequeueReusableCell(withIdentifier: HomeTableViewCell.reuseIdentifier, for: indexPath) as! HomeTableViewCell
-////            cell.goodsImage.image = homeGoodsDataArray[indexPath.row].goodsImage
-////            cell.goodsTitle.text = homeGoodsDataArray[indexPath.row].goodsTitle
-////            cell.locationLabel.text = homeGoodsDataArray[indexPath.row].locationLabel
-////            cell.goodsPrice.text = homeGoodsDataArray[indexPath.row].goodsPrice
-////            cell.ratingCustomView.talkImageView.image = homeGoodsDataArray[indexPath.row].rating.talkImage
-////            cell.ratingCustomView.talkCount.text = homeGoodsDataArray[indexPath.row].rating.talkNum
-////            cell.ratingCustomView.heartImageView.image = homeGoodsDataArray[indexPath.row].rating.heartImage
-////            cell.ratingCustomView.heartCount.text = homeGoodsDataArray[indexPath.row].rating.heartNum
-////            return cell
-////            
-////        }
-////    }
-//    
-//    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
-//        if indexPath.row == 6{
-//            return view.frame.width*0.8
-//            
-//        }else{
-//            return view.frame.width*0.37
-//        }
-//    }
-//    
-//    
-//}
-//
