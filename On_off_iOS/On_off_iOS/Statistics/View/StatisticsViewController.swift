e69f22aac328933b96560713045963f7788f7c5a
HJ <81843677+HJ39@users.noreply.github.com>
2024-01-18 00:35:26 +0900

    feat: 회고 작성률 View 구현

--- On_off_iOS/On_off_iOS/Statistics/View/StatisticsViewController.swift
+++ On_off_iOS/On_off_iOS/Statistics/View/StatisticsViewController.swift
@@ -22,6 +22,20 @@ final class StatisticsViewController: UIViewController {
         return label
     }()
     
+    /// 스크롤 뷰
+    private lazy var scrollView: UIScrollView = {
+        let view = UIScrollView()
+        view.backgroundColor = .clear
+        return view
+    }()
+    
+    /// Content View
+    private lazy var contentView: UIView = {
+        let view = UIView()
+        view.backgroundColor = .clear
+        return view
+    }()
+    
     /// 햇빛, 달 차트
     private lazy var monthChartView: DayChartCustomView = {
         let view = DayChartCustomView()
@@ -47,8 +61,27 @@ final class StatisticsViewController: UIViewController {
         view.backgroundColor = .clear
         return view
     }()
+     
+    /// 회고 작성 비율
+    private lazy var writeRateUIView: UIView = {
+        let view = UIView()
+        view.backgroundColor = .cyan
+        view.layer.cornerRadius = 20
+        return view
+    }()
+    
+    /// 회고 작성 비율 제목
+    private lazy var writeRateUILabel: UILabel = {
+        let label = UILabel()
+        label.backgroundColor = .clear
+        label.numberOfLines = 2
+        label.textAlignment = .center
+        label.textColor = .black
+        return label
+    }()
     
     private let viewModel: StatisticsViewModel = StatisticsViewModel()
+    private let disposeBag = DisposeBag()
     
     // MARK: - View Did Load
     override func viewDidLoad() {
@@ -61,11 +94,14 @@ final class StatisticsViewController: UIViewController {
     
     /// AddSubViews
     private func addSubViews() {
-        view.addSubview(weekChartUIView)
-        view.addSubview(weekChartTitleLabel)
         view.addSubview(monthChartTitleLabel)
-        view.addSubview(monthChartView)
-        
+        view.addSubview(scrollView)
+        scrollView.addSubview(contentView)
+        contentView.addSubview(weekChartUIView)
+        contentView.addSubview(weekChartTitleLabel)
+        contentView.addSubview(monthChartView)
+        contentView.addSubview(writeRateUIView)
+        writeRateUIView.addSubview(writeRateUILabel)
         constraints()
     }
     
@@ -76,25 +112,50 @@ final class StatisticsViewController: UIViewController {
             make.leading.equalToSuperview().offset(20)
         }
         
+        scrollView.snp.makeConstraints { make in
+            make.top.equalTo(monthChartTitleLabel.snp.bottom).offset(10)
+            make.bottom.horizontalEdges.equalToSuperview()
+        }
+        
+        contentView.snp.makeConstraints { make in
+            make.top.equalTo(scrollView.snp.top)
+            make.bottom.equalTo(scrollView.snp.bottom)
+            make.leading.equalTo(scrollView.snp.leading)
+            make.trailing.equalTo(scrollView.snp.trailing)
+            make.width.equalTo(scrollView.snp.width)
+        }
+        
         monthChartView.snp.makeConstraints { make in
-            make.top.equalTo(monthChartTitleLabel.snp.bottom).offset(20)
+            make.top.equalToSuperview().offset(10)
             make.leading.equalToSuperview().offset(20)
             make.trailing.equalToSuperview().offset(-20)
         }
         
-        weekChartUIView.transform = CGAffineTransform(rotationAngle: -.pi/2)
+        weekChartTitleLabel.snp.makeConstraints { make in
+            make.top.equalTo(monthChartView.snp.bottom).offset(40)
+            make.leading.equalToSuperview().offset(20)
+        }
         
+        weekChartUIView.transform = CGAffineTransform(rotationAngle: -.pi/2)
         weekChartUIView.snp.makeConstraints { make in
-            make.bottom.equalTo(view.safeAreaLayoutGuide).offset(-40)
+            make.top.equalTo(weekChartTitleLabel.snp.bottom).offset(20)
             make.centerX.equalToSuperview()
             make.width.equalTo(view.safeAreaLayoutGuide.layoutFrame.width)
             make.height.equalTo(weekChartUIView.snp.width).multipliedBy(0.7)
         }
         
-        weekChartTitleLabel.snp.makeConstraints { make in
-            make.bottom.equalTo(weekChartUIView.snp.top).offset(-30)
-            make.leading.equalToSuperview().offset(20)
+        writeRateUIView.snp.makeConstraints { make in
+            make.top.equalTo(weekChartUIView.snp.bottom).offset(50)
+            make.horizontalEdges.equalToSuperview().inset(20)
+            make.bottom.equalToSuperview()
+            make.height.equalTo(130)
         }
+        
+        writeRateUILabel.snp.makeConstraints { make in
+            make.horizontalEdges.equalToSuperview().inset(10)
+            make.verticalEdges.equalToSuperview().inset(20)
+        }
+        
     }
     
     /// binding
@@ -103,6 +164,7 @@ final class StatisticsViewController: UIViewController {
         
         bindWeekView(output: output)
         bindMonthView(output: output)
+        bindWriteRateUILabel(output: output)
     }
     
     /// binding WeekChartUIView Data
@@ -117,6 +179,16 @@ final class StatisticsViewController: UIViewController {
         guard let statistics = output.monthStatisticsRelay.value else { return }
         monthChartView.inputData(statistics: statistics)
         monthChartTitleLabel.text = output.monthTitleRelay.value
+    }
+    
+    /// binding Write Rate UILabel
+    private func bindWriteRateUILabel(output: StatisticsViewModel.Output) {
+        output.writeRateRelay
+            .bind { [weak self] title in
+                guard let self = self else { return }
+                writeRateUILabel.attributedText = title
+            }
+            .disposed(by: disposeBag)
     }
 }
 
--- On_off_iOS/On_off_iOS/Statistics/ViewModel/StatisticsViewModel.swift
+++ On_off_iOS/On_off_iOS/Statistics/ViewModel/StatisticsViewModel.swift
@@ -8,6 +8,7 @@
 import Foundation
 import RxSwift
 import RxRelay
+import UIKit
 
 final class StatisticsViewModel {
     
@@ -16,6 +17,7 @@ final class StatisticsViewModel {
         var monthStatisticsRelay: BehaviorRelay<MonthStatistics?> = BehaviorRelay(value: nil)
         var weekTitleRelay: BehaviorRelay<String> = BehaviorRelay(value: "")
         var weekStatisticsRelay: BehaviorRelay<DayStatistics?> = BehaviorRelay(value: nil)
+        var writeRateRelay: BehaviorRelay<NSMutableAttributedString?> = BehaviorRelay(value: nil)
     }
     
     /// Create Output
@@ -24,6 +26,7 @@ final class StatisticsViewModel {
         
         weekDummy(output: output)
         monthDummy(output: output)
+        writeRateDummy(output: output)
         return output
     }
     
@@ -43,5 +46,20 @@ final class StatisticsViewModel {
     private func monthDummy(output: Output) {
         output.monthTitleRelay.accept("2023년 11월")
         output.monthStatisticsRelay.accept(MonthStatistics(dayTime: 0.3, nightTime: 0.6))
+    }
+    
+    /// 회고 작성 비율 더미 데이터
+    private func writeRateDummy(output: Output) {
+        let title = "이번달 평균 회고 작성률은 PERCENT%"
+            .replacingOccurrences(of: "PERCENT", with: "50")
+        let subTitle = "\n조금만 더 힘내볼까요?✨"
+        
+        let attributedString = NSMutableAttributedString(string: title)
+        attributedString.addAttribute(.font, value: UIFont.boldSystemFont(ofSize: 20),
+                                      range: (title as NSString).range(of: title))
+        
+        attributedString.append(NSMutableAttributedString(string: subTitle))
+        
+        output.writeRateRelay.accept(attributedString)
     }
 }
