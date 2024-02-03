8a340ed62efe054cccdfa47495ec4385067be72f
yeahzxnn <yejin0621@tukorea.ac.kr>
2024-02-04 02:51:59 +0900

    feat : Initial View 제작 완료.

--- On_off_iOS/On_off_iOS/Home/TodayResolution/View/TodayResolutionViewController.swift
+++ On_off_iOS/On_off_iOS/Home/TodayResolution/View/TodayResolutionViewController.swift
@@ -21,17 +21,16 @@ final class TodayResolutionViewController: UIViewController {
         return view
     }()
     
-//    /// StackView - 오늘의 다짐, 오늘의 다짐 네비게이션 버튼
-//    private lazy var stackView: UIStackView = {
-//        let view = UIStackView(arrangedSubviews: [todayResolutionTitleLabel, todayResolutionButton])
-//        view.backgroundColor = .clear
-//        view.axis = .horizontal
-//        view.spacing = 5  // 또는 충돌하지 않는 적절한 값으로 변경
-//        view.distribution = .fillProportionally
-//        return view
-//    }()
-    
+    //contentView
     private lazy var contentView: UIView = {
+        let view = UIView()
+        view.backgroundColor = .clear
+        return view
+        
+    }()
+    
+    //오늘의 다짐 타이틀 + 버튼 넣는 View
+    private lazy var todayscontentView: UIView = {
         let view = UIView()
         view.backgroundColor = .clear
         return view
@@ -42,38 +41,44 @@ final class TodayResolutionViewController: UIViewController {
     private lazy var todayResolutionTitleLabel: UILabel = {
         let label = UILabel()
         label.text = "오늘의 다짐"
-        label.backgroundColor = .clear
-        label.font = UIFont(name: "Pretendard-SemiBold", size: 18)
+//        label.backgroundColor = .OnOffLightPurple
+        label.font = UIFont.pretendard(size: 25, weight: .bold)
         label.textColor = .OnOffPurple
+
+//        // 그림자 설정 -> 안 보임...
+//        label.layer.shadowColor = UIColor.OnOffLightPurple.cgColor
+//        label.layer.shadowOpacity = 0.5
+//        label.layer.shadowOffset = CGSize(width: 10, height: 4) // 수평 이동을 0으로 변경
+//        label.layer.shadowRadius = 8 // 그림자의 크기를 크게 조절
         return label
     }()
     
     //오늘의 다짐 네비게이션 버튼
     private lazy var todayResolutionButton: UIButton = {
         let button = UIButton()
-        button.backgroundColor = .clear
+        button.backgroundColor = .OnOffLightPurple
         button.tintColor = .OnOffPurple
-        button.setImage(UIImage(named: "chevron.right"), for: .normal)
+        button.setImage(UIImage(systemName: "chevron.right"), for: .normal)
         return button
     }()
+
     
-    /// 오늘의 다짐 contentVIew
-    private lazy var todayresolutioncontentView: UIView = {
+    /// 오늘의 다짐 띄워주는 뷰
+    private lazy var resolutioncontentView: UIView = {
         let view = UIView()
         view.layoutIfNeeded()
-        view.layer.cornerRadius = 15
+        view.layer.cornerRadius = 20
         view.clipsToBounds = true
-        view.backgroundColor = .clear
-        view.tintColor = .OnOffLightPurple
+        view.backgroundColor = .OnOffLightPurple
         return view
     }()
     
     //오늘의 다짐안에 들어갈 초기문구
-    private var todayresolutionwriteLabel: UILabel = {
+    private var resolutionwriteLabel: UILabel = {
         let label = UILabel()
         label.text = "오늘 지키고 싶은 다짐을 적어보세요!"
         label.backgroundColor = .clear
-        label.font = UIFont(name: "Pretendard-Medium", size: 14)
+        label.font = UIFont.pretendard(size: 15, weight: .medium)
         label.textColor = .lightGray
         return label
     }()
@@ -97,50 +102,63 @@ final class TodayResolutionViewController: UIViewController {
     private func addSubViews() {
         view.addSubview(scrollView)
         scrollView.addSubview(contentView)
-        contentView.addSubview(todayResolutionTitleLabel)
-        contentView.addSubview(todayResolutionButton)
-        scrollView.addSubview(todayresolutioncontentView)
-        todayresolutioncontentView.addSubview(todayresolutionwriteLabel)
+        contentView.addSubview(todayscontentView)
+        todayscontentView.addSubview(todayResolutionTitleLabel)
+        todayscontentView.addSubview(todayResolutionButton)
+        contentView.addSubview(resolutioncontentView)
+        resolutioncontentView.addSubview(resolutionwriteLabel)
+        
         
     }
     
     /// Set Constraints
     private func constraints() {
+        
         scrollView.snp.makeConstraints { make in
-            make.edges.equalToSuperview()
+            make.top.bottom.horizontalEdges.equalToSuperview()
         }
         
         contentView.snp.makeConstraints { make in
-            make.leading.trailing.equalToSuperview().offset(30)
-            make.top.equalToSuperview().offset(30)
-            make.height.equalTo(100)
+            make.top.equalTo(scrollView.snp.top)
+            make.bottom.equalTo(scrollView.snp.bottom)
+            make.leading.equalTo(scrollView.snp.leading)
+            make.trailing.equalTo(scrollView.snp.trailing)
+            make.width.equalTo(scrollView.snp.width)
+        }
+        
+        todayscontentView.snp.makeConstraints { make in
+            make.leading.trailing.equalTo(contentView)
+            make.top.equalTo(contentView).offset(30)
+            make.height.equalTo(80)
             
         }
         
         todayResolutionTitleLabel.snp.makeConstraints { make in
-            make.top.leading.equalTo(contentView).offset(20)
-            //top 위치 버튼이랑 같아야함., horizontal그거 다시
+            make.top.leading.equalTo(todayscontentView).offset(20)
         }
-
+        
         todayResolutionButton.snp.makeConstraints { make in
-          
-        }
-
-        
-        todayresolutioncontentView.snp.makeConstraints { make in
-            
+            make.leading.equalTo(todayResolutionTitleLabel.snp.trailing).offset(10)
+            make.centerY.equalTo(todayResolutionTitleLabel)
         }
         
-        todayresolutionwriteLabel.snp.makeConstraints { make in
-            make.center.equalTo(todayresolutioncontentView) // 중앙에 위치
-            make.edges.equalTo(todayresolutioncontentView).inset(UIEdgeInsets(top: 30, left: 30, bottom: 30, right: 30))
+        
+        resolutioncontentView.snp.makeConstraints { make in
+            make.top.equalTo(todayscontentView.snp.bottom).offset(10)
+            make.leading.equalTo(todayscontentView).offset(20)
+            make.trailing.equalTo(todayscontentView).offset(-20)
+            make.height.equalTo(200)
+        }
+        
+        resolutionwriteLabel.snp.makeConstraints { make in
+            make.centerX.centerY.equalTo(resolutioncontentView)
         }
         
     }
-
-
-
-
+    
+    
+    
+    
     
     
     //    /// Binding
